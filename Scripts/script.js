662ae7bf4513123da78ce4cb73d1e34039004095
RishabhTrivedi-22 <rishabhtrivedi695@gmail.com>
2024-03-03 18:18:37 +0530

    Login Page start

--- Scripts/script.js
+++ Scripts/script.js
@@ -10,6 +10,8 @@ const menu = document.querySelector(".menu");
 const menuNav = document.querySelector("#menu-link-container");
 const navItems = document.querySelectorAll("#menu-link-container li");
 const backBtn2 = d.querySelector("#productDescBox2 .backBtn");
+const loginWrapper = d.querySelector("#form-wrapper");
+const registerLink = d.querySelector(".register-link");
 
 
 let showMenu = true;
@@ -18,46 +20,49 @@ menuBtn.addEventListener("click", toggleMenu);
 
 
 productCards.forEach((card) => {
-  card.addEventListener("click", () => {
-    prodDescBox.classList.toggle("active-box");
-  });
+	card.addEventListener("click", () => {
+		prodDescBox.classList.toggle("active-box");
+	});
 });
 productCards2.forEach((card) => {
-  card.addEventListener("click", () => {
-    prodDescBox2.classList.toggle("active-box");
-  });
+	card.addEventListener("click", () => {
+		prodDescBox2.classList.toggle("active-box");
+	});
 });
 
 backBtn1.addEventListener("click", () => {
-  prodDescBox.classList.toggle("active-box");
-})
+	prodDescBox.classList.toggle("active-box");
+});
 
 backBtn2.addEventListener("click", () => {
-  prodDescBox2.classList.toggle("active-box");
-})
+	prodDescBox2.classList.toggle("active-box");
+});
 
-	
 
-	function toggleMenu() {
-		if (!showMenu) {
-			menuBtn.classList.add("close");
-			menu.classList.add("show");
-			menuNav.classList.add("show");
-			navItems.forEach((item) =>
-				item.classList.add("show"));
 
-			// Reset the menu state
-			showMenu = true;
-		} else {
-			menuBtn.classList.remove("close");
-			menu.classList.remove("show");
-			menuNav.classList.remove("show");
-			navItems.forEach((item) =>
-				item.classList.remove("show"));
+function toggleMenu() {
+	if (!showMenu) {
+		menuBtn.classList.add("close");
+		menu.classList.add("show");
+		menuNav.classList.add("show");
+		navItems.forEach((item) =>
+			item.classList.add("show"));
 
-			// Reset the menu state
-			showMenu = false;
-		}
+		// Reset the menu state
+		showMenu = true;
+	} else {
+		menuBtn.classList.remove("close");
+		menu.classList.remove("show");
+		menuNav.classList.remove("show");
+		navItems.forEach((item) =>
+			item.classList.remove("show"));
+
+		// Reset the menu state
+		showMenu = false;
 	}
+}
 
- 
+
+registerLink.addEventListener("click", () => {
+	loginWrapper.classList.add('signupactive');
+});
